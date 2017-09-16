665fa8998c9b7f083644cb79b29f73dcc9726225
cipepser <respepic@gmail.com>
2017-09-16 10:54:47 +0900

    add a test of Sigmoid

--- myfunc/myfunc_test.go
+++ myfunc/myfunc_test.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gonum/matrix/mat64"
 )
 
+func TestSigmoid(t *testing.T) {
+	expected := 0.5
+	actual := Sigmoid(0)
+
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+
+}
+
 func TestVec2Slice(t *testing.T) {
 	expected := []float64{1, 2, 3, 4}
 	v := mat64.NewVector(len(expected), expected)
